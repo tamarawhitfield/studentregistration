1ef88975d7fb0041f7761f02a34164d8c8e79df0
tamarawhitfield <94706691+tamarawhitfield@users.noreply.github.com>
2024-08-25 11:51:06 -0500

    Add files via upload

---
+++ landing.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Student Course System</title>
+    <link rel="stylesheet" href="style.css"> <!-- Link to the CSS file -->
+</head>
+<body>
+    <h1>Welcome Student!</h1>
+    <p>Have Control Over Your Courses</p>
+    <a href="login.php">Login</a>
+    <a href="register.php">Enroll Now</a>
+</body>
+</html>
